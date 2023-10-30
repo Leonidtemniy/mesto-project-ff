d8a9e9cc3846495790c70eb906cf295e3efdd8d0
Leonid Temniy <leonidtemniy@yandex.ru>
2023-10-30 23:55:16 +0300

    начал работу над 5 ПР

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
-
+const templateEl = document.querySelector('#card-template');
+const cardsContainer = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-
+function createCard(name, link) {
+  const newCardEl = templateEl.cloneNode(true);
+  const newCardTitle = document.querySelector('.card__title');
+  const newCardImage = document.querySelector('.card__image');
+  newCardTitle.textContent = name;
+  newCardImage.src = link;
+  newCardImage.alt = 'Фотография' + name;
+  return newCardEl;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard() {}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => {
+  newCardEl = createCard(card);
+  cardsContainer.append(newCardEl);
+});
