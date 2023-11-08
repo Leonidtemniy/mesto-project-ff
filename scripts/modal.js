823da8e79b864be5fe62d547e5ee9ec94b885912
Leonid Temniy <leonidtemniy@yandex.ru>
2023-11-08 23:35:01 +0300

    последние правки с плавным открытием

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,12 @@
 //в файле modal.js описаны функции для работы с модальными окнами: функция открытия модального окна, функция закрытия модального окна, функция-обработчик события нажатия Esc и функция-обработчик события клика по оверлею
+//==============Глобальные перенменные для реализации попапов==========//
 const profileEditBtn = document.querySelector('.profile__edit-button');
 const profileAddBtn = document.querySelector('.profile__add-button');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popTypeNewCard = document.querySelector('.popup_type_new-card');
 const allPopups = document.querySelectorAll('.popup');
 
+//=================Функции реализации открытия/закрытия попапов=========//
 profileEditBtn.addEventListener('click', () => {
   openPopup(popupTypeEdit);
 });
@@ -13,11 +15,17 @@ profileAddBtn.addEventListener('click', () => {
 });
 
 export function openPopup(popUpSelector) {
-  popUpSelector.classList.add('popup_is-opened');
+  popUpSelector.classList.add('popup_is-animated');
+  setTimeout(() => {
+    popUpSelector.classList.add('popup_is-opened');
+  }, 1);
   addEventListener();
 }
 function closePopup(popUpSelector) {
   popUpSelector.classList.remove('popup_is-opened');
+  setTimeout(() => {
+    popUpSelector.classList.remove('popup_is-opened');
+  }, 1);
   removeEventListener();
 }
 
@@ -54,6 +62,7 @@ allPopups.forEach(popup => {
 
 const cardImgs = document.querySelectorAll('.card__image');
 const popupTypeImage = document.querySelector('.popup_type_image');
+
 cardImgs.forEach(cardImg => {
   cardImg.addEventListener('click', () => {
     openPopup(popupTypeImage);
@@ -61,6 +70,8 @@ cardImgs.forEach(cardImg => {
     const popupCaption = popupTypeImage.querySelector('.popup__caption');
     bigImg.src = cardImg.src;
     bigImg.alt = cardImg.alt;
-    popupCaption.textContent = bigImg.alt;
+    popupCaption.textContent = bigImg.alt.replace('Фотография', '');
   });
 });
+//=================Реализация логики работы форм============///
+//const editForm = document.forms.
